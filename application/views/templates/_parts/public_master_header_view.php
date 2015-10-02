aaa4db4727bdf1a75cd7153eba59351f06d49569
sam_tunebox <sam.wong@caterhammedia.com>
2015-10-02 17:48:32 +0800

    hard code nav bar

--- application/views/templates/_parts/public_master_header_view.php
+++ application/views/templates/_parts/public_master_header_view.php
@@ -24,37 +24,23 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand"
-                   href="<?php echo site_url();?>"><?php echo $website->title;?></a>
+                   href="<?php echo site_url();?>">roKKi</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <?php echo $top_menu;?>
+                    <li><a href="#">Chats</a></li>
+                    <li><a href="#">Inflight Menu</a></li>
                 </ul>
-                <?php
-                if(sizeof($langs)>1) {
-                    ?>
+ 
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false"><?php echo $current_lang['name'];?> <span
-                                    class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Hi Sami<span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
-                                <?php
-                                foreach($langs as $slug => $language)
-                                {
-                                    if($slug!=$current_lang['slug']) {
-                                        echo '<li>' . anchor($language['alternate_link'], $language['name']) . '</li>';
-                                    }
-                                ?>
-                                <?php
-                                }
-                                ?>
+                                <li><a href="#">Help</a></li>
+                                <li><a href="#">Logout</a></li>
                             </ul>
                         </li>
                     </ul>
-                <?php
-                }
-                    ?>
             </div>
             <!--/.nav-collapse -->
         </div>
